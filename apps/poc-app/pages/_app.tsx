24e5a2212c19cf39237faa31c0cccbfc669923fb
PublicisSapientSid <siddhartha.chatterjee@publicissapient.com>
2022-03-08 20:21:27 +0530

    Initial commit

--- apps/poc-app/pages/_app.tsx
+++ apps/poc-app/pages/_app.tsx
@@ -6,7 +6,7 @@ function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Welcome to demo-app!</title>
+        <title>Welcome to poc-app!</title>
       </Head>
       <main className="app">
         <Component {...pageProps} />
