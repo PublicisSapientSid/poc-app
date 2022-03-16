5dab0c92bb2fe78c0b79ba1b2d57442ab3b8d9f4
PublicisSapientSid <siddhartha.chatterjee@publicissapient.com>
2022-03-16 16:37:11 +0530

    Initialize new application

--- libs/slider/src/lib/slider.tsx
+++ libs/slider/src/lib/slider.tsx
@@ -2,7 +2,7 @@ import { ChangeEvent, useState } from 'react';
 import styles from './slider.module.scss';
 
 export interface sliderLegend {
-  dataType: 'text' | 'image';
+  isText: boolean;
   data: string[];
 }
 
@@ -21,7 +21,7 @@ export interface SliderProps {
 }
 
 const textSliderLegends: sliderLegend = {
-  dataType: 'text',
+  isText: true,
   data: [
     'Not at all',
     'Very less',
@@ -34,7 +34,7 @@ const textSliderLegends: sliderLegend = {
 };
 
 export const emojiSliderLegends: sliderLegend = {
-  dataType: 'image',
+  isText: false,
   data: [
     'https://cdn.shopify.com/s/files/1/1061/1924/files/Very_Angry_Emoji_60x60.png?2976903553660223024',
     'https://cdn.shopify.com/s/files/1/1061/1924/files/Disappointed_Face_Emoji_60x60.png?8026536574188759287',
@@ -63,30 +63,28 @@ export function Slider({
   const maxLength = sliderLegends.data.length - 1;
   max = Math.min(max, maxLength);
 
-  const previewValue =
-    sliderLegends.dataType === 'text' ? (
-      <h2 className={styles['value-text']}>
-        {sliderLegends.data[Math.round(sliderVal)]}
-      </h2>
-    ) : (
-      <img
-        className={styles['preview-value']}
-        src={sliderLegends.data[Math.round(sliderVal)]}
-      />
-    );
+  const previewValue = sliderLegends.isText ? (
+    <h2 className={styles['value-text']}>
+      {sliderLegends.data[Math.round(sliderVal)]}
+    </h2>
+  ) : (
+    <img
+      className={styles['preview-value']}
+      src={sliderLegends.data[Math.round(sliderVal)]}
+    />
+  );
 
-  const rangeIndicators =
-    sliderLegends.dataType === 'text' ? (
-      <>
-        <p>{sliderLegends.data[min]}</p>
-        <p>{sliderLegends.data[max]}</p>
-      </>
-    ) : (
-      <>
-        <img src={sliderLegends.data[min]} />
-        <img src={sliderLegends.data[max]} />
-      </>
-    );
+  const rangeIndicators = sliderLegends.isText ? (
+    <>
+      <p>{sliderLegends.data[min]}</p>
+      <p>{sliderLegends.data[max]}</p>
+    </>
+  ) : (
+    <>
+      <img src={sliderLegends.data[min]} />
+      <img src={sliderLegends.data[max]} />
+    </>
+  );
 
   return (
     <div className={`${styles[sliderLength]} ${styles['slider-container']}`}>
